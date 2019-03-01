2272875332c9b5e9ff49a3e53f28feb7d5349fa3
Воронцов Олег Валерьевич <vorontsov@root.ians.aero>
2019-03-01 12:13:38 +0300

    Первый залив

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Omni
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.coursValue1 = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bankOmni = new System.Windows.Forms.Label();
+            this.BankBtc = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.coursValue1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Курс OMNI/BTC:";
+            // 
+            // coursValue1
+            // 
+            this.coursValue1.DecimalPlaces = 2;
+            this.coursValue1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.coursValue1.Location = new System.Drawing.Point(112, 20);
+            this.coursValue1.Name = "coursValue1";
+            this.coursValue1.Size = new System.Drawing.Size(71, 20);
+            this.coursValue1.TabIndex = 1;
+            this.coursValue1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.coursValue1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.bankOmni);
+            this.panel1.Controls.Add(this.BankBtc);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(1, 144);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(283, 77);
+            this.panel1.TabIndex = 2;
+            // 
+            // bankOmni
+            // 
+            this.bankOmni.AutoSize = true;
+            this.bankOmni.Location = new System.Drawing.Point(22, 52);
+            this.bankOmni.Name = "bankOmni";
+            this.bankOmni.Size = new System.Drawing.Size(53, 13);
+            this.bankOmni.TabIndex = 2;
+            this.bankOmni.Text = "OMNI: 10";
+            // 
+            // BankBtc
+            // 
+            this.BankBtc.AutoSize = true;
+            this.BankBtc.Location = new System.Drawing.Point(22, 23);
+            this.BankBtc.Name = "BankBtc";
+            this.BankBtc.Size = new System.Drawing.Size(40, 13);
+            this.BankBtc.TabIndex = 1;
+            this.BankBtc.Text = "BTC: 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Мои Запасы";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Продажа btc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Текущее значение курса:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(225, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Предыдущее критическое значение курса:";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Location = new System.Drawing.Point(1, 68);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(283, 73);
+            this.panel2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 226);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.coursValue1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.coursValue1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown coursValue1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label bankOmni;
+        private System.Windows.Forms.Label BankBtc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Omni
+{
+    public partial class Form1 : Form
+    {
+        //это глобальные переменные на форме
+        float courseOld;    //предыдущий курс (увязан с шагом step. То есть при колебании курса менее шага это значение не изменится, а при 0,1 - изменится)
+        float courseNew;    //новый курс
+        float step;         //значение критического шага (у тебя это 0,1)
+
+        float ownBtc;       //сколько имеем битков
+        float ownOmni;      //сколько у нас омни
+        
+        public Form1()
+        {            
+            InitializeComponent();
+            //задаем стартовые значения
+            button1.Visible = false;             //это кнопка сбрасывает значение курса до 1. пока делаем невидимой
+            courseOld = (float)coursValue1.Value; //начальные значения курсов
+            courseNew = (float)coursValue1.Value;
+            ownOmni = 10;                        //чем располагаем в начале
+            ownBtc = 0;
+            BankBtc.Text = "BTC: " + ownBtc;     //Выведим то, чем располагаем в label
+            bankOmni.Text = "Omni " + ownOmni;
+            step = (float)0.1;                   //задаем величину критичности шага в 0,1
+            coursValue1.Increment=0.05M;          //задаем шаг изменения курса (при нажатии на бегунок значение курса увеличиватся/уменьшается на это значение)
+        }
+
+        //ставим обработчик на событие изменения значения формы курса
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //если курс дошел до 2, то сделать неактивным и сделать видимой кнопку сброса уровня курса до 1Omni=1Btc.
+            if (coursValue1.Value >= 2) {
+                coursValue1.Enabled = false;
+                button1.Visible = true;
+            }
+
+            //обновляем значение нового значения курса, ведь в форме поменялось значение
+            //значение старое обновляем в том случае, если дельта между ними будет >= 0.1
+            courseNew =  (float)coursValue1.Value; 
+
+
+            //смотрим дельту между старым и новым значением курса. Round для более правильного округления.
+            float dif = (float)Math.Round((float)((courseNew - courseOld)), 2);
+
+            //если dif положительная, то курс Omni к битку растет и это первый кейс (продажа omni). Но только в том случае если дельта превышает или равно заявленным 0,1
+            if (dif >= 0)
+            {
+                if ((courseOld != courseNew) & (dif >= step))
+                {
+                    if (courseOld < courseNew)
+                    {
+                        //MessageBox.Show("Продавать 0,1 омни");
+                        //функция (описана ниже) по продаже OMNI                         
+                        SellMyOmni();
+                    }
+                    //дельта превышает значение 0.1 => теперь в старое значение записывается текущее новое значение.
+                    courseOld = courseNew;
+
+                }
+                //отображаем для наглядности текщие значения courseOld и courseNew
+                label5.Text = "Предыдущее критическое значение курса " + courseOld.ToString() + ".";
+                label6.Text = "Текущее значение курса " + courseNew.ToString() + ".";
+            }
+            //если dif отрицательная, то курс Битка растет по отношнию к Omni и это второй кейс (продажа битков)
+            else
+            {
+                if ((courseOld != courseNew) & (Math.Abs(dif) >= step))
+                {
+                    if (courseOld > courseNew)
+                    {
+                        //MessageBox.Show("Продавать 1 биток");
+                        //Функция продажи битков
+                        SellMyBtc();
+                    }
+
+                    //отображаем для наглядности текщие значения courseOld и courseNew
+                    courseOld = courseNew;
+                }
+                //отображаем для наглядности текщие значения courseOld и courseNew
+                label5.Text = "Предыдущее критическое значение курса " + courseOld.ToString() + ".";
+                label6.Text = "Текущее значение курса " + courseNew.ToString() + ".";
+            }
+        }
+
+        //тут логика продажи Оmni при увеличении его курса
+        void SellMyOmni()
+        {
+            //есть омни - есть торговля)
+            if (ownOmni > 0)
+            {
+                //продаем 1 омни
+                ownOmni = ownOmni - 1;
+                //Увеличиваем значение наших битков на 1 омни (по курсу битка)
+                ownBtc += (float)coursValue1.Value;
+                //Отобразить на форме результат операции
+                BankBtc.Text = "BTC: " + ownBtc;
+                bankOmni.Text = "Omni " + ownOmni;
+            }
+            else
+                MessageBox.Show("Omni кончились!");
+        }
+
+        //логика продажи битков при уменьшении его курса
+        void SellMyBtc()
+        {
+            //Есть битки - есть продажи
+            if (ownBtc > 0)
+            {
+                //Продаем битков по цене 1 омни
+                ownBtc = ownBtc - (float)coursValue1.Value;
+                //Покупаем 1 омни
+                ownOmni = ownOmni + 1;
+                
+                //Отобразить на форме результат операции
+                BankBtc.Text = "BTC: " + ownBtc;
+                bankOmni.Text = "Omni " + ownOmni;
+            }
+            else
+                MessageBox.Show("Btc кончились!");
+        }
+
+        //обработчик кнопочки, обнуляющей значение курсов. 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            courseOld = 0;
+            courseNew = 0;
+            coursValue1.Value = 1;
+            coursValue1.Enabled = true;
+            button1.Visible = false; 
+        }      
+      }
+}
